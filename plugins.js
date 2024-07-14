5f9aef496c39316e3843af6a9ec39b95b2bdbe84
MariaWaBot <167265485+MariaWaBot@users.noreply.github.com>
2024-07-14 07:39:09 +0530

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -174,7 +174,7 @@ Maria.ev.on("connection.update",async  (s) => {
 console.log(chalk.green('🟨Welcome to Maria-md'));
 console.log(chalk.gray('\n\n🚀Initializing...'));
            await delay(1000 * 2) 
-            Maria.groupAcceptInvite("FGPKxVnjgJ7KnBGiDeb4ij")
+            
             
 console.log(chalk.cyan('\n\n🥵Connected'));
 
